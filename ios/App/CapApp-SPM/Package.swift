2403936a286772bb8ebbe114e0e1a2ff7df09650
novogram21 <moder.moni10@gmail.com>
2026-04-07 18:12:34 +0000

    Смена workspace на project

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.0"),
+        .package(name: "CapacitorKeyboard", path: "../../../node_modules/@capacitor/keyboard"),
         .package(name: "CordovaPluginAudiotoggle", path: "../../capacitor-cordova-ios-plugins/sources/CordovaPluginAudiotoggle"),
         .package(name: "CordovaPluginProximitySensor", path: "../../capacitor-cordova-ios-plugins/sources/CordovaPluginProximitySensor"),
         .package(name: "Cordova.plugins.diagnostic", path: "../../capacitor-cordova-ios-plugins/sources/Cordova.plugins.diagnostic")
@@ -22,6 +23,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
                 .product(name: "CordovaPluginAudiotoggle", package: "CordovaPluginAudiotoggle"),
                 .product(name: "CordovaPluginProximitySensor", package: "CordovaPluginProximitySensor"),
                 .product(name: "Cordova.plugins.diagnostic", package: "Cordova.plugins.diagnostic")
